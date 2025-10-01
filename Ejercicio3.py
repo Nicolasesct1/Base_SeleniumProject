ddbeea1bb5f329692275720ff0253853f2da7a55
Nicolas <nicolasesct99@gmail.com>
2025-10-01 04:16:56 -0500

    Limpiar codigo de time.sleep

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -29,17 +29,16 @@ new_popup_link.send_keys("https://practicum-content.s3.us-west-1.amazonaws.com/n
 popup_button = WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, ".//form[@name='new-card']/button[text()='Save']")))
 popup_button.click()
 delete_button = WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located((By.XPATH, "//li[@class='places__item card'][1]/button[@class='card__delete-button card__delete-button_visible']")))
+
 #Usar la variable new_title en {} ya que es dinamico
 new_title_pub = WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located((By.XPATH, f"//h2[@class='card__title' and text()='{new_title}']")))
-time.sleep(10)
 assert new_title_pub.text == new_title
 cards_before= len(driver.find_elements(By.CSS_SELECTOR, "li.places__item.card"))
 print(cards_before)
 delete_button.click()
-time.sleep(10)
+
 # Lambda es una funcion temporal para contar las cartas anteriores -1 en este caso (asi comprueba la eliminacion)
 WebDriverWait(driver, 10).until(lambda driver: len(driver.find_elements(By.CSS_SELECTOR, "li.places__item.card")) == cards_before - 1)
-time.sleep(10)
 cards_after= len(driver.find_elements(By.CSS_SELECTOR, "li.places__item.card"))
 print(cards_after)
 assert cards_after == cards_before - 1
